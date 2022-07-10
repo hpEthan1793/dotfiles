006eb7388377b586dac803affcf5c2bd8815a905
Ethan <50517186+hpEthan1793@users.noreply.github.com>
2022-07-10 12:24:24 +0200

    Delete fish directory

--- fish/config.fish
+++
@@ -1,4 +0,0 @@
-set -g -x fish_greeting 'Hi Ethan'
-alias serve="browser-sync start --server --files ."
-alias servey="yarn dev ."
-alias update="sudo apt update -y && sudo apt upgrade -y"
--- fish/functions/fzf_key_bindings.fish
+++
@@ -1,1 +0,0 @@
-/home/ethan/.fzf/shell/key-bindings.fish
